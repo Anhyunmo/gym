dfa210ecde4799fd290c77852784fb26a2614aff
dksah79 <dksah79@naver.com>
2022-02-23 11:18:08 +0900

    Order부분 수정

--- src/main/java/com/example/gym/domain/Order.java
+++ src/main/java/com/example/gym/domain/Order.java
@@ -25,9 +25,11 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "start_date")
     private Timestamp startDate;
 
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "finish_date")
     private Timestamp finishDate;
 
@@ -39,20 +41,27 @@ public class Order {
     @JoinColumn(name = "member_id", foreignKey = @ForeignKey(name="FK_MEMBER_TB_ORDER"))
     private Member member;
 
-    @Column
-    private String status;
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
-    public Order(Gym gym, Member member, Timestamp startDate, Timestamp finishDate, String status){
+    public Order(Gym gym, Member member, Timestamp startDate, Timestamp finishDate, Status status){
         setOrder(gym,member,startDate,finishDate,status);
     }
 
-    public Order setOrder(Gym gym, Member member, Timestamp startDate, Timestamp finishDate, String status){
+    public Order setOrder(Gym gym, Member member, Timestamp startDate, Timestamp finishDate, Status status){
         this.gym = gym;
         this.member = member;
         this.startDate = startDate;
         this.finishDate = finishDate;
         this.status = status;
         return this;
+    }
+
+    public enum Status{
+        사용완료,
+        사용중,
+        취소완료;
     }
 
 }
