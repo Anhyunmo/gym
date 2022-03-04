f11104eed3481b897d408a88bae75f33979e6e9a
dksah79 <dksah79@naver.com>
2022-03-04 09:26:22 +0900

    config

---
+++ src/main/java/com/example/gym/config/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package com.example.gym.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private String version;
+    private String title;
+
+    @Bean
+    public Docket apiV1() {
+        version = "v1";
+        title = "KOSTA Order " + version;
+
+        ParameterBuilder aParameterBuilder = new ParameterBuilder();
+        aParameterBuilder.name("X-AUTH-TOKEN") //헤더 이름
+                .description("Access Tocken") //설명
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> aParameters = new ArrayList<>();
+        aParameters.add(aParameterBuilder.build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .globalOperationParameters(aParameters)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.gym"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .apiInfo(apiInfo(title, version));
+
+    }
+
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfo(
+                title,
+                "KOSTA Order",
+                version,
+                "http://localhost:4000/",
+                new Contact("Contact Me", "", "dksah97@hanmail.com"),
+                "Licenses",
+                "",
+                new ArrayList<>());
+    }
+}
